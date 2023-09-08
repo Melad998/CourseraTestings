2fe043bc35b1409d051c1546deafe983ed642c22
Melad998 <118486834+Melad998@users.noreply.github.com>
2023-09-09 01:18:02 +0300

    Update DessertsList.js

--- DessertsList.js
+++ DessertsList.js
@@ -1,9 +1,5 @@
 function DessertsList(props) {
-  const desserts = [
-               {key:1,name:"ice cream",cal:200},
-               {key:2,name:"Tiramisu",cal:300},
-               {key:3,name:"Chocolate Cake",cal:400}
-]
+  
   return (
       <ul>
       {
