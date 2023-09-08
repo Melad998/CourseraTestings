db462925539175a576c311796698ed53b21e3121
Melad998 <118486834+Melad998@users.noreply.github.com>
2023-09-09 01:13:10 +0300

    Update DessertsList.js

--- DessertsList.js
+++ DessertsList.js
@@ -1,6 +1,19 @@
 function DessertsList(props) {
-  // Implement the component here.
-  return null;
+  const desserts = [
+               {key:1,name:"ice cream",cal:200},
+               {key:2,name:"Tiramisu",cal:300},
+               {key:3,name:"Chocolate Cake",cal:400}
+]
+  return (
+      <ul>
+      {
+       desserts.map(desser => {return(
+           <li>{desser.name} - {desser.cal} cal</li>
+)})
+      }
+      </ul>
+
+);
 }
 
 export default DessertsList;
